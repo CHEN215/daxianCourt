876e79ebade91e52089c1bb1aac910fd2620f5cf
CHEN215 <327441405@qq.com>
2017-07-02 19:40:19 +0800

    增加侧栏
    增加侧栏

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -4,12 +4,18 @@ return array(
     //数据库配置信息
     'DB_TYPE'   => 'mysqli', // 数据库类型
     'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'tonjiju', // 数据库名
+    'DB_NAME'   => 'zqcool', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => '', // 密码
+    'DB_PWD'    => 'root', // 密码
     'DB_PORT'   => 3306, // 端口
     'DB_PREFIX' => 'jl_', // 数据库表前缀
     'DB_CHARSET'=> 'utf8', // 字符集
     'AUTH_SUPERADMIN' => 'admin',//超级管理员
     
+    // wechat 配置
+    'WECHAT' => array(
+        'appid' => '',
+        'secret' => '',
+        //'token' => '',//用不上
+    )
 );
